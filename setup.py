b7ce091ff3438d94aca7f2f8e329b87c8663b875
Atanu Paul <38816880+Atanu-Paul-au6@users.noreply.github.com>
2020-06-09 00:48:12 +0530

    v0.5 distribution link

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ setup(
     # How you named your package folder (MyLib)
     name='bitcoin_price_alert_app',
     packages=['bitcoin_price_alert_app'],   # Chose the same as "name"
-    version='0.4',      # Start with a small number and increase it with every change you make
+    version='0.5',      # Start with a small number and increase it with every change you make
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
-    description='A python app to check the current price of bitcoin and send notifications and emails',
+    description='A python app that fetches the current price of bitcoins and sends notifications via Telegram, IFTTT, Email, twitter, and SMS',
     author='Atanu Paul',                   # Type in your name
     author_email='atanu.paul.rims@gmail.com',      # Type in your E-Mail
     # Provide either the link to your github or to your website
     url='https://github.com/Atanu-Paul-au6/bitcoin_price_alert_app',
     # I explain this later on
-    download_url='https://github.com/Atanu-Paul-au6/bitcoin_price_alert_app/archive/v0.4.tar.gz',
+    download_url='https://github.com/Atanu-Paul-au6/bitcoin_price_alert_app/archive/v0.5.tar.gz',
     # Keywords that define your package best
-    keywords=['PYTHON', 'BITCOIN', 'TELEGRAM', 'IFTTT', 'EMAIL'],
+    keywords=['PYTHON', 'BITCOIN', 'TELEGRAM', 'IFTTT', 'EMAIL','Twitter','Android-SMS'],
     install_requires=[            # I get to this in a second
         'requests'
     ],
@@ -24,7 +24,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         # Define that your audience are developers
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Notifications Application',
         'License :: OSI Approved :: MIT License',   # Again, pick a license
         # Specify which pyhton versions that you want to support
         'Programming Language :: Python :: 3',
